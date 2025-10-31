d934565b50e30b8994e30476a897775b90617aaa
Josh Allen <largozor@josha.au>
2025-10-31 12:57:36 +1100

    MM-4.12-connected-trap-signals-via-loop
    Added the traps group and connected signals using the group

--- scripts/level.gd
+++ scripts/level.gd
@@ -6,7 +6,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var traps = get_tree().get_nodes_in_group("traps")
+	for trap in traps:
+		# trap.connect("touched_player", _on_trap_touched_player)
+		trap.touched_player.connect(_on_trap_touched_player)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
