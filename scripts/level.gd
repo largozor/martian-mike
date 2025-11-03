6ed416b436e09bd71f9a2f0457cdc6b81603d7d4
Josh Allen <largozor@josha.au>
2025-11-03 13:19:58 +1100

    MM-4.16-get-player-from-group
    Get the player reference from the players group

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
-@onready var player = $Player
+
 @onready var start_position = $StartPosition
 
+var player = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	player.global_position = start_position.get_spawn_position()
+	player = get_tree().get_first_node_in_group("player")
+	if player != null:
+		player.global_position = start_position.get_spawn_position()
 	var traps = get_tree().get_nodes_in_group("traps")
 	for trap in traps:
 		# trap.connect("touched_player", _on_trap_touched_player)
